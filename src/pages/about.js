ddd09c8cc91aab6b1bde5bcabb7c898abfbc4757
JohnAlbin <virtually.johnalbin@gmail.com>
2019-11-27 10:36:36 +0100

    Add about CSS

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import styles from './about.module.css';
 
 import Layout from '../components/layout'
 
 const AboutPage = () => (
   <Layout>
-    <h1>About</h1>
+    <h1 className={styles.Heading}>About</h1>
     <p>Let’s grab this data from a Markdown file.</p>
     <Link to="/">Home</Link>
   </Layout>
